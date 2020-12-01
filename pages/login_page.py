0cd9492b81639d742d81bba6e3f092dbaf3e600e
Roman Shpak <rshpak1996@gmail.com>
2020-12-01 20:14:06 +0200

    fix login_page tabs

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,16 +3,13 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
-        def should_be_login_url(self):
-        # реалізуйте перевірку на корректну адресу
-        assert "login" in self.browsere.current_url, "Current page is not a login page"
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "Current page is not a login page"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM)
 
     def should_be_login_page(self):
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 # pytest -v --tb=line --language=en test_main_page.py
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,4 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)      # ініціалізуєм Page Object, передаєм в конструктор екземпляр драйвера і url адресу
     page.open()                         # відкриваєм сторінку
     page.should_be_login_link()         # перевіряємо наявнність посилання на сторінку логіна
-    # page.go_to_login_page()             # виконуєм метод сторінки - переходимо на сторінку логіна
+    page.go_to_login_page()             # виконуєм метод сторінки - переходимо на сторінку логіна
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
