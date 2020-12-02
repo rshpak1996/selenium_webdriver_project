282dc532584272158a34f4ab09733b25781803df
Roman Shpak <rshpak1996@gmail.com>
2020-12-02 14:58:35 +0200

    add test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -34,26 +34,39 @@ import pytest
 #     product_page.should_be_equal_price()
 
 
-@pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
+# @pytest.mark.xfail
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.add_to_basket()
+#     product_page.should_not_be_success_message()
+
+
+# def test_guest_cant_see_success_message(browser):
+#     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.should_not_be_success_message()
+
+
+# @pytest.mark.xfail
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.add_to_basket()
+#     product_page.should_disappeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
     product_page.open()
-    product_page.add_to_basket()
-    product_page.should_not_be_success_message()
+    product_page.should_be_login_link()
 
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
     product_page.open()
-    product_page.should_not_be_success_message()
-
-
-@pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_to_basket()
-    product_page.should_disappeared_success_message()
+    product_page.go_to_login_page()
