119a2302bb3b632a58e2c00ba5cd446b931fed4d
Roman Shpak <rshpak1996@gmail.com>
2020-12-03 14:02:20 +0200

    final prepare for review

--- test_product_page.py
+++ test_product_page.py
@@ -8,13 +8,14 @@ import pytest
 # pytest -v --tb=line --language=en test_product_page.py
 
 
-def test_user_cant_see_success_message(browser):
+def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_not_be_success_message()
 
-def test_user_can_add_product_to_basket(browser):
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
     product_page.open()
@@ -68,12 +69,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     product_page.open()
     product_page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/"
     product_page = ProductPage(browser, link)
@@ -114,6 +117,7 @@ class TestUserAddToBasketFromProductPage():
         product_page.open()
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         product_page = ProductPage(browser, link)
